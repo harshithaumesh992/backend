f32c58b7cd8d7ab080283c9df2fc6df28a96c8df
Harshitha K U <harshithaumesh771@gmail.com>
2026-04-13 14:55:21 +0530

    herjehr

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const dns = require("dns");
 dotenv.config();
+
+dns.setServers(["1.1.1.1", "8.8.8.8"]);
 
 const cors = require("cors");
 const Razorpay = require("razorpay");
